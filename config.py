10a26747e4d6597f7e4bbd2df354b6f3a88c677d
FizeriDeveloperOfficial <efimrota1@gmail.com>
2026-07-09 01:43:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # Токен твоего бота (получи у @BotFather в Telegram)
-BOT_TOKEN = "8997942123:AAGSe9xzhEq22PiyUXBXULZorIZYTM8fLTw"
+BOT_TOKEN = "YOUR BOT TOKEN"
 
 # Юзернейм твоего канала (например, "@my_music_channel") 
 # Бот должен быть назначен АДМИНИСТРАТОРОМ в этом канале!
-CHANNEL_ID = "@MusicNaLuboiVkuss"
+CHANNEL_ID = "YOUR CHANNEL ID WITH BOT ADMINISTRATOR"
 
 # Папка для сохранения музыки
 DOWNLOAD_FOLDER = "Music"
